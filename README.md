369709041bba0f40853fc71bab6e2cbd03e0d73a
Franco <jfranco8@outlook.com>
2022-06-27 15:02:44 -0500

    First draft

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # Ubuntu
+Welcome to my personal Ubuntu installation script. In this particular document, I try to install most of the software I do use after a clean install of this awesome Linux distribution. Ideas and suggestions were collected from different sources and also from personal experience. 
+
+
+
+## How to install:
+Copy and paste this commands to your terminal:
+
+  1. clone the script
+  ```bash 
+      git clone https://github.com/solosoyfranco/Ubuntu.git && cd ./Ubuntu
+  ```
+  2. make it executable & run it
+  ```bash 
+      chmod +x ./install.sh
+      ./install.sh
+  ```
+
+
+## Screenshots
+
+![install.sh](https://github.com/solosoyfranco/Ubuntu/blob/main/screenshot1.png?raw=true "install.sh")
+
+![d3100](https://github.com/solosoyfranco/Ubuntu/blob/main/screenshot2.png?raw=true "Dell DisplayLink D3100 - 2x 25' dell 2k screens")
+
+
+ 
+
+
+## Apps (Option 10)
+
+* neofetch
+* cmatrix
+* htop
+* p7zip
+* gparted
+* Visual Studio Code
+* Notion app enhanced
+* VLC
+* google chrome stable
+* microsoft edge
+* brave browser
+* discord
+* remmina
+* pomodoro timer
+* Microsoft Teams
+* Spotify
+* GIMP
+* GNOME Network Display
+* Evolution email (great for o365)
+* Inkscape
+* Whatsapp
+* Filezilla
+* Parsec
+* qBittorrent
+* Thunderbird
+* Dolphin file
+* Disk Usage Analyzer
+* Atom
+* PyCharm Pro
+* SQL client
+* Prusa Slicer
+* Ultimaker Cura
+* Firmware
+* QR Decoder
+* Bluetooth Manager
+
+
+
+### Recommended tabs & Sources
+* https://extensions.gnome.org/extension/307/dash-to-dock/
+* https://extensions.gnome.org/extension/5004/dash-to-dock-for-cosmic/
+* https://extensions.gnome.org/extension/19/user-themes/
+* https://extensions.gnome.org/extension/779/clipboard-indicator/
+* https://extensions.gnome.org/extension/1262/bing-wallpaper-changer/
+* https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+* https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
+* https://extensions.gnome.org/extension/4245/gesture-improvements/
+* https://extensions.gnome.org/extension/517/caffeine/
+* https://extensions.gnome.org/extension/3780/ddterm/
+* https://extensions.gnome.org/extension/2741/remove-alttab-delay-v2/
+* https://extensions.gnome.org/extension/4679/burn-my-windows/
+* https://extensions.gnome.org/extension/805/hide-dash/
+* https://extensions.gnome.org/extension/2087/desktop-icons-ng-ding/
+* https://extensions.gnome.org/extension/1723/wintile-windows-10-window-tiling-for-gnome/
+* https://extensions.gnome.org/extension/4325/hide-activities-button/
+* https://extensions.gnome.org/extension/1460/vitals/
+* https://extensions.gnome.org/extension/277/impatience/
+* https://extensions.gnome.org/extension/2/move-clock/
+* https://extensions.gnome.org/extension/708/panel-osd/
+* https://extensions.gnome.org/extension/3843/just-perfection/
+* https://extensions.gnome.org/extension/8/places-status-indicator/
+* https://extensions.gnome.org/extension/905/refresh-wifi-connections/
+* https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
+* #https://extensions.gnome.org/extension/945/cpu-power-manager/
+* https://extensions.gnome.org/extension/570/todotxt/
+* https://extensions.gnome.org/extension/770/force-quit/
+* https://extensions.gnome.org/extension/3193/blur-my-shell/
+* #https://extensions.gnome.org/extension/4158/gnome-40-ui-improvements/
+* https://extensions.gnome.org/extension/3730/floating-dock/
+* https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
+* https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
+* https://extensions.gnome.org/extension/1034/services-systemd/
+* https://extensions.gnome.org/extension/120/system-monitor/
+* https://extensions.gnome.org/extension/1080/transparent-notification/
+* https://extensions.gnome.org/extension/302/windowoverlay-icons/*
--- install.sh
+++ install.sh
@@ -1,76 +1,140 @@
-install minimal and add 3rd party
-sudo apt update && sudo apt upgrade
-sudo apt install gnome-shell-extensions
-sudo apt autoremove
-sudo apt install -y gnome-tweaks
-sudo apt install snapd
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.github.debauchee.barrier
-sudo apt install ubuntu-restricted-extras
-sudo apt install ubuntu-restricted-extras libdvd-pkg ubuntu-restricted-addons
-sudo apt install rar unrar p7zip-full p7zip-rar
-#cat /proc/sys/vm/swappiness
-#sudo nano /etc/sysctl.conf
-# vm.swappiness=10 ### add at the end
-sudo apt install gnome-sushi
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-sudo apt-get install synaptic
-sudo add-apt-repository ppa:gerardpuig/ppa
-sudo apt-get update
-sudo apt-get install ubuntu-cleaner
-sudo apt install git
-git clone https://github.com/vinceliuice/Orchis-theme.git
-####https://github.com/daniruiz/Flat-Remix-GTK
-sudo add-apt-repository ppa:daniruiz/flat-remix
-sudo apt update
-sudo apt install flat-remix-gtk
-sudo add-apt-repository ppa:daniruiz/flat-remix
-sudo apt update
-sudo apt install flat-remix
-https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
-sudo snap install curl
-bash -c "$(curl -sLo- https://git.io/JvvDs)"
-https://drasite.com/blog/Pimp%20my%20terminal
-sudo apt  install tree
-sudo snap install htop
-sudo snap install btop
-sudo apt install curl wget git
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-$ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-$ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-nano ~/.zshrc
-###ZSH_THEME="agnoster"
-###ZSH_THEME="powerlevel10k/powerlevel10k"
-###plugins=(git docker python zsh-autosuggestions zsh-syntax-highlighting)
-source ~/.zshrc
-#### https://www.tecmint.com/install-oh-my-zsh-in-ubuntu/
-chsh -s /usr/bin/zsh
-sudo apt-get install fonts-powerline
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-p10k configure
-sudo apt-get install dconf-cli
-git clone https://github.com/dracula/gnome-terminal
-cd gnome-terminal
-./install.sh
-#https://medium.com/@satriajanaka09/setup-zsh-oh-my-zsh-powerlevel10k-on-ubuntu-20-04-c4a4052508fd
+#!/bin/bash
+# fedora-scripts
+# This is free software, and you are welcome to redistribute it
+# under certain conditions
+# Licensed under GPLv3 License
 
 
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+HEIGHT=20
+WIDTH=90
+CHOICE_HEIGHT=4
+BACKTITLE="Ubuntu 20.04 - 22.04 Post-Install - By Franco Lopez - https://github.com/solosoyfranco"
+TITLE="Make a selection"
+MENU="Please choose one of the following options:"
 
 
-#spotify 
-           sudo flatpak install -y flathub com.spotify.Client
+#Check to see if Dialog is installed, if not install it
+if [ $(deb -q dialog 2>/dev/null | grep -c "is not installed") -eq 1 ]; then
+sudo apt install -y dialog
+fi
+
+OPTIONS=(1  "System Update"
+         2  "Enable Flatpak, Tweaks, Media Codecs, Extensions & Extras"
+         3  "Swappiness - 10"
+         4  "Appearance Tweaks - Flat GTK and Icon Theme"
+         5  "Install Nvidia Drivers"
+         6  "Install KVM virtualization software"
+         7  "Enable Better Fonts - Better font rendering"
+         8  "Install Common Software - Installs a bunch of my most used software"
+         9  "Install DisplayLink Dock (d3100)"
+         10 "Install Oh-My-ZSH w pl10k"
+         11 "Reboot"
+         12 "Quit")
+
+while [ "$CHOICE -ne 4" ]; do
+    CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --nocancel \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+    clear
+    case $CHOICE in
+        1)
+            echo "System Update"
+            sudo apt update && sudo apt upgrade
+            sudo apt autoremove
+            notify-send "Option 1 - system update" --expire-time=10
+            ;;
+        2)
+            echo "Enable Flatpak, Tweaks, Media Codecs, Extensions & Extras"
+            ##\
+            sudo apt update
+            sudo apt install -y gnome-shell-extensions gnome-tweaks snapd flatpak gnome-software-plugin-flatpak ubuntu-restricted-extras libdvd-pkg ubuntu-restricted-addons synaptic git curl wget gnome-shell-extension-appindicator blueman neofetch apt-transport-https p7zip p7zip-full unzip rar unrar p7zip-rar gparted clang cmake ubuntu-cleaner tree htop gnome-sushi gpg
+            sudo snap install btop
+            sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            notify-send "Option 2 - tweaks" --expire-time=10
+            ;;
+        3)  echo "Swappiness - 10"
+            #####
+            #####cat /proc/sys/vm/swappiness 
+            #sudo nano /etc/sysctl.conf
+            # vm.swappiness=10 ### add at the end
+            ####
+            notify-send "Option 3 - Swappiness - 10"--expire-time=10
+            ;;
+        4)  echo "Appearance Tweaks - Flat GTK and Icon Theme"
+            sudo add-apt-repository ppa:daniruiz/flat-remix
+            sudo apt install -y gnome-shell-extension-user-theme flat-remix-gtk flat-remix paper-icon-theme flat-remix-icon-theme flat-remix-theme 
+            gnome-extensions install user-theme@gnome-shell-extensions.gcampax.github.com
+            gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+            gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue-Dark-Solid"
+            gsettings set org.gnome.desktop.wm.preferences theme "Flat-Remix-Blue"
+            gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue'
+            gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+            gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+            notify-send "Option 4 - There you go, that's better" --expire-time=10
+            ;;
+        5)  echo "Nvidia drivers"
+            modinfo -F version nvidia
+            sudo add-apt-repository ppa:graphics-drivers/ppa
+            sudo apt update -y # and reboot if you are not on the latest kernel
+            sudo ubuntu-drivers autoinstall
+            ubuntu-drivers devices
+            sudo apt install -y nvidia-driver-510
+            notify-send "Option 5 - Nvidia Drivers" --expire-time=10
+            ;;
+        6)  echo "KVM virtualization software"
+            sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
+            sudo systemctl enable --now libvirtd
+            notify-send "Option 6 - fKVM installed" --expire-time=10            
+            ;;
+        7)  echo "Enabling Better Fonts"
+            ## need to work on this
+            # sudo -s apt -y copr enable dawid/better_fonts
+            # sudo -s apt install -y fontconfig-font-replacements
+            # sudo -s apt install -y fontconfig-enhanced-defaults
+            notify-send "Option 7 - Fonts prettified - enjoy!" --expire-time=10
+            ;;
+        8)  echo "Installing Software"
+            #necessary software
+            sudo flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            sudo apt install -y apt-transport-https git gpg curl wget fonts-noto libxss1 libindicator7 ttf-mscorefonts-installer
+            #notion repo
+            echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /etc/apt/sources.list.d/notion-repackaged.list
+            #VSC repo
+            wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+            sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+            sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+            rm -f packages.microsoft.gpg
+            #microsoft edge repo
+            curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+            sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+            sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
+            sudo rm microsoft.gpg
+            #brave repo
+            sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+            #microsoft teams repo
+            curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+            sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+            #remmina repo
+            sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+            
+            #install discord
+            sudo flatpak install -y flathub com.discordapp.Discord
+            #spotify 
+            sudo flatpak install -y flathub com.spotify.Client
             #Gimp
             sudo flatpak install -y flathub org.gimp.GIMP
             #network displays
             sudo flatpak install -y flathub org.gnome.NetworkDisplays
-            #######evolution email
-            #####sudo flatpak install -y flathub org.gnome.Evolution
-            #agenda to do
-            sudo flatpak install -y flathub com.github.dahenson.agenda
-            
+            #evolution email
+            sudo flatpak install -y flathub org.gnome.Evolution
             #inkscape
             sudo flatpak install -y flathub org.inkscape.Inkscape
             #Whatsapp
@@ -83,7 +147,6 @@ cd gnome-terminal
             sudo flatpak install -y flathub org.qbittorrent.qBittorrent
             #thunderbild mail
             sudo flatpak install -y flathub org.mozilla.Thunderbird
-            
             # Disk analyzer
             sudo flatpak install -y flathub org.gnome.baobab
             # Atom
@@ -92,90 +155,52 @@ cd gnome-terminal
             sudo flatpak install -y flathub com.jetbrains.PyCharm-Professional
             # SQL client 
             sudo flatpak install -y flathub com.github.alecaddd.sequeler
-            #Teamviewer
-            
             # Firmware
             sudo flatpak install -y flathub org.gnome.Firmware
             # Decoder 
             sudo flatpak install -y flathub com.belmoussaoui.Decoder
             #ultimaker cura 
             sudo flatpak install -y flathub com.ultimaker.cura
+            #barrier
+            sudo flatpak install -y flathub com.github.debauchee.barrier
+
+            #Teamviewer
+            wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && sudo apt install -y ./teamviewer_amd64.deb
+            # chrome
+            wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt -f -y install ./google-chrome-stable_current_amd64.deb
+
+            # install software
+            sudo fc-cache -vr
+            sudo apt-get check
+            sudo apt update
+            sudo apt install -y microsoft-edge-stable code teams brave-browser notion-app-enhanced remmina remmina-plugin-rdp remmina-plugin-secret gnome-shell-pomodoro vlc cmatrix prusa-slicer
+            sudo apt autoremove
+            notify-send "Option 8 - Software has been installed" --expire-time=10
+            ;;
+        9) echo "Install DisplayLink Dock (d3100)"
+            #https://www.synaptics.com/products/displaylink-graphics/downloads/ubuntu
             
-            sudo apt install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator vlc mscore-fonts-all google-noto-sans-fonts neofetch cmatrix p7zip unzip gparted google-chrome-stable clang cmake microsoft-edge-stable code htop brave-browser notion-app-enhanced gnome-pomodoro prusa-slicer 
-            
-            dnfdragora
-            teams
-            blueman
-            
-            
-            sudo apt install -y gnome-tweaks gnome-shell-extension-appindicator vlc neofetch cmatrix p7zip unzip gparted clang cmake prusa-slicer
-
-
-
-
-
-
-########################
-
-
-
-E: Unable to locate package gnome-extensions-app
-sudo apt install ttf-mscorefonts-installer
-sudo fc-cache -vr
-sudo apt-get install fonts-noto
-sudo apt-get install libxss1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt -f install
-
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
-sudo rm microsoft.gpg
-sudo apt update && sudo apt install microsoft-edge-stable
-
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
-
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
-
-echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /etc/apt/sources.list.d/notion-repackaged.list
-sudo apt update
-sudo apt install notion-app-enhanced
-
-
-sudo apt-get install gnome-shell-pomodoro
-https://extensions.gnome.org/extension/805/hide-dash/
-https://extensions.gnome.org/extension/2087/desktop-icons-ng-ding/
-https://extensions.gnome.org/extension/1723/wintile-windows-10-window-tiling-for-gnome/
-https://extensions.gnome.org/extension/4325/hide-activities-button/
-https://extensions.gnome.org/extension/1460/vitals/
-https://extensions.gnome.org/extension/277/impatience/
-https://extensions.gnome.org/extension/2/move-clock/
-https://extensions.gnome.org/extension/708/panel-osd/
-https://extensions.gnome.org/extension/3843/just-perfection/
-https://extensions.gnome.org/extension/8/places-status-indicator/
-https://extensions.gnome.org/extension/905/refresh-wifi-connections/
-https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
-#https://extensions.gnome.org/extension/945/cpu-power-manager/
-https://extensions.gnome.org/extension/570/todotxt/
-https://extensions.gnome.org/extension/770/force-quit/
-https://extensions.gnome.org/extension/3193/blur-my-shell/
-#https://extensions.gnome.org/extension/4158/gnome-40-ui-improvements/
-https://extensions.gnome.org/extension/3730/floating-dock/
-https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
-https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
-https://extensions.gnome.org/extension/1034/services-systemd/
-https://extensions.gnome.org/extension/120/system-monitor/
-https://extensions.gnome.org/extension/1080/transparent-notification/
-https://extensions.gnome.org/extension/302/windowoverlay-icons/
+            notify-send "Option 9 - Done" --expire-time=10
+            ;;
+        10) echo "Installing Oh-My-Zsh"
+            sudo apt install -y zsh fonts-powerline dconf-cli
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+            git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+            chsh -s /usr/bin/zsh
+            sudo nano ~/.zshrc
+            #ZSH_THEME="powerlevel10k/powerlevel10k"
+            #plugins=(git docker python zsh-autosuggestions zsh-syntax-highlighting)
+            #p10k configure
+            notify-send "Option 11 - Oh-My-Zsh is ready to rock n roll" --expire-time=10
+            ;;
+        
+        11)
+            sudo systemctl reboot
+            ;;
+        12)
+          exit 0
+          ;;
+    esac
+done
